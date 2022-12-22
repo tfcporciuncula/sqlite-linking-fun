74d60871331b032e9162de28aaf397f69b2f75f1
Fred Porciúncula <tfcporciuncula@gmail.com>
2022-12-22 17:22:07 +0100

    Add blank KMM project with CocoaPods

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "sqlite_linking"
+include(":androidApp")
+include(":shared")
