2a58834cfb12f251c5e1d5649293869e6cc9c85b
Fred Porciúncula <tfcporciuncula@gmail.com>
2022-12-29 15:24:23 +0100

    Break the build trying to access Sentry on iosMain

--- shared/src/iosMain/kotlin/com/fredporciuncula/sqlitelinking/Platform.kt
+++ shared/src/iosMain/kotlin/com/fredporciuncula/sqlitelinking/Platform.kt
@@ -1,6 +1,7 @@
 package com.fredporciuncula.sqlitelinking
 
 import platform.UIKit.UIDevice
+import cocoapods.Sentry
 
 class IOSPlatform: Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
